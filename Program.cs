5e674e9319ca925c454dfaaee08ff3e11ea02c52
Kenny Kropp <istylzer@gmail.com>
2013-06-11 19:22:46 +0300

    Added "login" Function.

--- Program.cs
+++ Program.cs
@@ -8,14 +8,18 @@ namespace BreadBox
     class Program
     {
         static String INFO = "BreadBox\n=======\n\nOpen Source.\nhttps://github.com/EpicZerg/BreadBox\n=========================================";
-        static String HELP = "";
+        static String HELP_A = "Commands:\ninfo : Displays Information\nhelp : Displays this\nlogin username password : Logs you in for the current Session\n";
+        static String HELP_B = "";
+        static String DB_USER_TABLE = "LoginCredentials";
+        static String DB_HOST = "", DB_USER = "", DB_PASS = "", DB_DATABASE = "";
+        static Boolean loggedIn = false;
         static void Main(string[] args)
         {
             
             if (args.Length != 0)
             {
                 DataBaseHandler dbh = new DataBaseHandler();
-                dbh.connect("HOST", "USER", "PASSWORD");
+                dbh.connect(DB_HOST, DB_USER, DB_PASS, DB_DATABASE);
                 switch (args[0])
                 {
                     case "info":
@@ -25,17 +29,24 @@ namespace BreadBox
                         }
                     case "help":
                         {
-                            write(HELP);
+                            write(HELP_A + HELP_B);
                             break;
                         }
-                    case "permlogin":
+                    case "login":
                         {
                             if (args.Length > 2)
-                                dbh.login(args[1], args[2]);
+                                if (dbh.login(args[1], args[2], DB_USER_TABLE))
+                                    loggedIn = true;
+                                else
+                                    write("Login wrong, or DB is down.");
                             break;
                         }
-
-                }
+                    default:
+                        {
+                            write(HELP_A + HELP_B);
+                            break;
+                        }
+                   }
             }
         }
         static void write(String s)
