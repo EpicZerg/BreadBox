8f48daa0b2b5fa9c9baecc2af4d605e3a7211073
Kenny Kropp <istylzer@gmail.com>
2013-06-11 19:03:05 +0300

    Newly added

---
+++ DataBaseHandler.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BreadBox
+{
+    class DataBaseHandler
+    {
+        internal void connect(string host, string user, string password)
+        {
+
+        }
+        internal void login(string username, string password)
+        {
+           
+        }
+    }
+}
