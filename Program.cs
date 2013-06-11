1eed56ba15d8eeff01f92d0e927c79e422dcf4c6
Kenny Kropp <istylzer@gmail.com>
2013-06-11 19:02:14 +0300

    Added Basic Arguments

--- Program.cs
+++ Program.cs
@@ -7,8 +7,40 @@ namespace BreadBox
 {
     class Program
     {
+        static String INFO = "BreadBox\n=======\n\nOpen Source.\nhttps://github.com/EpicZerg/BreadBox\n=========================================";
+        static String HELP = "";
         static void Main(string[] args)
         {
+            
+            if (args.Length != 0)
+            {
+                DataBaseHandler dbh = new DataBaseHandler();
+                dbh.connect("HOST", "USER", "PASSWORD");
+                switch (args[0])
+                {
+                    case "info":
+                        {
+                            write(INFO);
+                            break;
+                        }
+                    case "help":
+                        {
+                            write(HELP);
+                            break;
+                        }
+                    case "permlogin":
+                        {
+                            if (args.Length > 2)
+                                dbh.login(args[1], args[2]);
+                            break;
+                        }
+
+                }
+            }
+        }
+        static void write(String s)
+        {
+            Console.Out.Write(s);
         }
     }
 }
